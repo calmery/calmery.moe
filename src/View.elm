010d50d8b42b1c6f91c8b153a0fb3d6925ee70f7
Marei Kikukawa <contact@calmery.me>
2019-07-07 02:24:27 +0900

    fullpage.js を削除してレイアウトを整える

--- src/View.elm
+++ src/View.elm
@@ -1,33 +1,24 @@
 module View exposing (view)
 
 import Browser exposing (Document)
-import Html exposing (Html, a, div, h1, p, text)
-import Html.Attributes exposing (class, href, id)
+import Html exposing (Html, a, div, h1, header, img, node, p, text)
+import Html.Attributes exposing (alt, class, href, id, src)
 import Model exposing (Model)
-import Pages.Articles as Articles
-import Pages.Goods as Goods
-import Pages.Profile as Profile
-import Pages.Top as Top
+import Parts.Header as Header
 import Route exposing (Route(..))
 import Update exposing (Msg(..))
 
 
 view : Model -> Document Msg
 view model =
-    { title = "Elm App"
+    { title = "Calmery.moe"
     , body =
-        [ div
-            [ id "fullPage" ]
-            [ section <| Top.view model
-            , section <| Profile.view model
-            , section <| Articles.view model
-            , section <| Goods.view model
+        [ node "main"
+            []
+            [ div
+                [ id "container" ]
+                [ Header.view model
+                ]
             ]
         ]
     }
-
-
-section : Html Msg -> Html Msg
-section html =
-    div [ class "section" ]
-        [ html ]
