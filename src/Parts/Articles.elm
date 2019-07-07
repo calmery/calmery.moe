83d6768944d30b04d17770da4c8e0739e2b365cb
Marei Kikukawa <contact@calmery.me>
2019-07-07 16:23:46 +0900

    記事へのリンクのレイアウトを整える

--- src/Parts/Articles.elm
+++ src/Parts/Articles.elm
@@ -1,7 +1,7 @@
 module Parts.Articles exposing (view)
 
-import Html exposing (Html, div, section, text)
-import Html.Attributes exposing (id)
+import Html exposing (Html, a, div, p, section, text)
+import Html.Attributes exposing (class, href, id)
 import Model exposing (Model)
 import Update exposing (Msg)
 
@@ -10,4 +10,15 @@ view : Model -> Html Msg
 view model =
     section
         [ id "articles" ]
-        []
+        [ article, article, article ]
+
+
+article =
+    div
+        [ class "article" ]
+        [ div [ class "title" ] [ text "かるめりちゃんスタンプ" ]
+        , div [ class "date" ] [ text "2019/01/30" ]
+        , div [ class "thumbnail" ] []
+        , p [ class "description" ] [ text "イラストはめたねのおくすりさん！（@metaneno）" ]
+        , div [ class "readmore" ] [ a [ href "" ] [ text "続きを読む" ] ]
+        ]
