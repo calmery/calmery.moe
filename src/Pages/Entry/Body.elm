a08ef75aec23def238a8aa4b8517242d900fbd3a
Marei Kikukawa <contact@calmery.me>
2019-07-09 00:48:25 +0900

    記事の画像を表示するように変更する

--- src/Pages/Entry/Body.elm
+++ src/Pages/Entry/Body.elm
@@ -1,16 +1,32 @@
 module Pages.Entry.Body exposing (view)
 
-import Html exposing (Html, div, p, text)
-import Html.Attributes exposing (id)
+import Html exposing (Html, a, div, img, p, text)
+import Html.Attributes exposing (alt, class, href, id, src, style)
 import Markdown
-import Model exposing (Entry)
+import Model exposing (Attachment, Entry)
 import Msg exposing (Msg)
 
 
 view : Entry -> Html Msg
 view entry =
-    div [ id "body" ]
-        [ p
-            []
-            [ Markdown.toHtml [] entry.body ]
+    div
+        []
+        [ Markdown.toHtml [ id "body" ] entry.body
+        , attachmentsView entry.attachments
+        ]
+
+
+attachmentsView : List Attachment -> Html Msg
+attachmentsView attachments =
+    div [ id "attachments" ] (List.map attachmentView attachments)
+
+
+attachmentView : Attachment -> Html Msg
+attachmentView attachment =
+    a [ href attachment.url ]
+        [ div
+            [ class "attachment" ]
+            [ p [ class "attachment-title" ] [ text attachment.title ]
+            , div [ class "attachment-image", style "background" ("url(" ++ attachment.thumbnail ++ ") center / cover") ] []
+            ]
         ]
