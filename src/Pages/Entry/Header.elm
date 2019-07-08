cfa7a9950493d10dbb4994b95850c1b11f5c03b9
Marei Kikukawa <contact@marei.jp>
2019-07-08 23:25:19 +0900

    本文の表示と elm-explorations/markdown の追加をする

---
+++ src/Pages/Entry/Header.elm
@@ -0,0 +1,31 @@
+module Pages.Entry.Header exposing (view)
+
+import Html exposing (Html, a, div, h1, header, img, node, p, text)
+import Html.Attributes exposing (alt, href, id, src)
+import Markdown
+import Model exposing (Entry)
+import Msg exposing (Msg)
+
+
+view : Entry -> Html Msg
+view entry =
+    header
+        []
+        [ div [ id "logo-and-links" ] [ title entry ]
+        , border
+        ]
+
+
+title : Entry -> Html Msg
+title entry =
+    h1
+        []
+        [ text entry.title
+        ]
+
+
+border : Html Msg
+border =
+    div
+        [ id "border" ]
+        []
