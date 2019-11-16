e2852aee090beacc59e5632b19f22164f985d237
Marei Kikukawa <contact@calmery.me>
2019-11-16 20:25:25 +0900

    プロフィールの自己紹介を更新する

--- src/Parts/Footer/Profile.elm
+++ src/Parts/Footer/Profile.elm
@@ -44,4 +44,4 @@ description : Html Msg
 description =
     p
         [ id "description" ]
-        [ text "田舎の大学院生．JavaScript，Elm とか好き．イラストを投稿するサービスの会社でエンジニアしてます．" ]
+        [ text "JavaScript と Elm、銀髪赤眼の女の子が好き。イラスト SNS の会社でソフトウェアエンジニアしてます。" ]
