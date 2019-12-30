b36b5bfff0b5b2b622abe7ffb7024652b295a90e
Marei Kikukawa <contact@calmery.me>
2019-12-30 16:26:07 +0900

    Redux を削除する

--- src/containers/Counter/Counter.test.tsx
+++ src/containers/Counter/Counter.test.tsx
@@ -1,16 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Counter from "./Counter";
-import { store } from "../../modules";
-import { Provider } from "react-redux";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(
-    <Provider store={store}>
-      <Counter />
-    </Provider>,
-    div
-  );
+  ReactDOM.render(<Counter />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
