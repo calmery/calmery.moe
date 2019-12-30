4515c35b2bc0c4067318f687100db8819201b555
Marei Kikukawa <contact@calmery.me>
2019-12-30 14:58:15 +0900

    Sentry にエラーログを送るようにする

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,19 @@ import * as ReactDOM from "react-dom";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter as Router } from "connected-react-router";
+import * as Sentry from "@sentry/browser";
 import { store, history } from "./modules";
 import * as serviceWorker from "./serviceWorker";
 import "./index.scss";
 import Top from "./pages/Top";
 
 serviceWorker.register();
+
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://35c3c6bfa928420a80d0c15d8a7a1a34@sentry.io/1868485"
+  });
+}
 
 const Routes = () => (
   <Switch>
