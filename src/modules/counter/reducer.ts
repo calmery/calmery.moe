e4667412d300fc7bb3b09f628d57835ea29f1046
Marei Kikukawa <contact@calmery.me>
2019-12-30 15:27:05 +0900

    Google Analytics にイベントを送るようにする

--- src/modules/counter/reducer.ts
+++ src/modules/counter/reducer.ts
@@ -1,4 +1,9 @@
 import { INCREMENT, DECREMENT, Actions } from "./actions";
+import {
+  sendToGoogleAnalytics,
+  GOOGLE_ANALYTICS,
+  GOOGLE_ANALYTICS_ACTION
+} from "../../helpers/google-analytics";
 
 export type CounterState = {
   count: number;
@@ -11,8 +16,17 @@ const initialState: CounterState = {
 export default (state = initialState, action: Actions) => {
   switch (action.type) {
     case INCREMENT:
+      sendToGoogleAnalytics(
+        GOOGLE_ANALYTICS.COUNTER,
+        GOOGLE_ANALYTICS_ACTION.COUNTER_INCREMENT,
+        action.payload.count.toString()
+      );
       return { count: state.count + action.payload.count };
     case DECREMENT:
+      sendToGoogleAnalytics(
+        GOOGLE_ANALYTICS.COUNTER,
+        GOOGLE_ANALYTICS_ACTION.COUNTER_DECREMENT
+      );
       return { count: state.count - 1 };
     default:
       return state;
