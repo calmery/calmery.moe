a01612b298ed084dad3444831071021acc9a30db
Marei Kikukawa <contact@calmery.me>
2020-01-05 01:40:43 +0900

    不要なコンポーネントを削除する

--- src/containers/Counter/Counter.spec.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { Counter } from "./Counter";
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Counter />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
