e285d6fe7511eff7b2381a8a51110d57b213127f
Marei Kikukawa <contact@calmery.me>
2020-01-05 21:26:35 +0900

    SNS にシェアボタンからシェアできるようにする

--- src/components/FooterShareButtons/FooterShareButtons.tsx
+++ src/components/FooterShareButtons/FooterShareButtons.tsx
@@ -3,8 +3,26 @@ import styles from "./FooterShareButtons.scss";
 
 export const FooterShareButtons: React.FC = () => (
   <div className={styles.container}>
-    <img src="images/share-buttons/twitter.svg" alt="Twitter" />
-    <img src="images/share-buttons/line.svg" alt="LINE" />
-    <img src="images/share-buttons/github.svg" alt="GitHub" />
+    <a
+      href="http://twitter.com/share?url=https%3A%2F%2Fcalmery.moe&text=Calmery.moe"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src="images/share-buttons/twitter.svg" alt="Twitter" />
+    </a>
+    <a
+      href="http://line.me/R/msg/text/?Calmery.moe%20https%3A%2F%2Fcalmery.moe"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src="images/share-buttons/line.svg" alt="LINE" />
+    </a>
+    <a
+      href="https://github.com/calmery/calmery.moe"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src="images/share-buttons/github.svg" alt="GitHub" />
+    </a>
   </div>
 );
