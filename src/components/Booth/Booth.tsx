fe176c8053b222e2486b48cdc03c2c3f0fb9dbe8
Marei Kikukawa <contact@calmery.me>
2020-01-10 01:07:52 +0900

    BOOTH のアイテムを取得したデータを元に表示する

--- src/components/Booth/Booth.tsx
+++ src/components/Booth/Booth.tsx
@@ -3,19 +3,21 @@ import { Card } from "~/components/Card";
 import { HorizontalScrollView } from "~/components/HorizontalScrollView";
 import { Logo, LogoService } from "~/components/Logo";
 import styles from "./Booth.scss";
+import data from "~/data/booth.json";
 
 export const Booth: React.FC = () => (
   <React.Fragment>
     <Logo service={LogoService.BOOTH} />
     <HorizontalScrollView className={styles.container}>
-      {[
-        "images/booth/1.jpg",
-        "images/booth/2.jpg",
-        "images/booth/3.jpg",
-        "images/booth/4.jpg",
-        "images/booth/5.jpg"
-      ].map((url, index) => (
-        <Card className={styles.booth} key={index} thumbnail={url} />
+      {data.map(({ id }, index) => (
+        <a
+          href={`https://calmery.booth.pm/items/${id}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          key={index}
+        >
+          <Card className={styles.booth} thumbnail={`images/booth/${id}.png`} />
+        </a>
       ))}
     </HorizontalScrollView>
   </React.Fragment>
