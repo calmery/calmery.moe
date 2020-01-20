5aa4749202f115ca02ad97ffeb9417aa464f7c27
Marei Kikukawa <contact@calmery.me>
2020-01-21 02:28:18 +0900

    freepik のライセンス情報を追加する

--- src/components/FooterComment/FooterComment.tsx
+++ src/components/FooterComment/FooterComment.tsx
@@ -4,5 +4,9 @@ import styles from "./FooterComment.scss";
 export const FooterComment: React.FC = () => (
   <div className={styles.container}>
     Made with <img src="images/heart.svg" alt="ハート" /> by Calmery
+    <br />
+    <a href="https://www.freepik.com/free-photos-vectors/background">
+      Background vector created by freepik - freepik.com
+    </a>
   </div>
 );
