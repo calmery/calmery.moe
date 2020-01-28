018628417024eb75d3fd2c0ba7c174180733cd69
Marei Kikukawa <contact@calmery.me>
2020-01-29 00:44:19 +0900

    BOOTH のアイテム表示部分を BoothItem コンポーネントとして切り出す

--- src/containers/Booth/Booth.tsx
+++ src/containers/Booth/Booth.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { useEffect, useRef, useState } from "react";
-import { Card } from "~/components/Card";
+import { BoothItem as BoothItemView } from "~/components/BoothItem";
 import { HorizontalScrollView } from "~/components/HorizontalScrollView";
 import { HorizontalScrollViewItem } from "~/containers/HorizontalScrollViewItem";
 import { Logo, LogoService } from "~/components/Logo";
@@ -8,7 +8,7 @@ import { getBooth, BoothItem } from "~/helpers/api";
 import styles from "./Booth.scss";
 
 export const Booth: React.FC = () => {
-  const [data, setData] = useState<BoothItem[] | null>(null);
+  const [data, setData] = useState<BoothItem[]>([]);
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -29,35 +29,19 @@ export const Booth: React.FC = () => {
     };
   }, []);
 
-  const rootRefCurrent = ref.current;
-
   return (
     <React.Fragment>
       <Logo service={LogoService.BOOTH} />
       <HorizontalScrollView rootRef={ref} className={styles.container}>
-        {/* TODO: 表示領域前後でフェードイン、フェードアウトさせるために必要、もう少し良い方法を考えたい */}
-        {rootRefCurrent &&
-          data &&
+        {ref.current &&
           data.map(({ id, thumbnailImageUrl }, index) => (
             <HorizontalScrollViewItem
               key={index}
-              rootRefCurrent={rootRefCurrent}
+              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+              rootRefCurrent={ref.current!}
               className={styles.item}
             >
-              <a
-                href={`https://calmery.booth.pm/items/${id}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Card
-                  className={styles.booth}
-                  thumbnail={[
-                    {
-                      url: thumbnailImageUrl[0]
-                    }
-                  ]}
-                />
-              </a>
+              <BoothItemView id={id} thumbnailImageUrl={thumbnailImageUrl[0]} />
             </HorizontalScrollViewItem>
           ))}
       </HorizontalScrollView>
