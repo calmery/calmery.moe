bbc5f3a2002ee881fae71545ab2a18b25dbd5b76
Marei Kikukawa <contact@calmery.me>
2020-03-28 04:45:51 +0900

    開発中のバッジを別途表示するように変更する

--- src/components/RelatedServicesItem/RelatedServicesItem.tsx
+++ src/components/RelatedServicesItem/RelatedServicesItem.tsx
@@ -1,15 +1,26 @@
 import * as React from "react";
 import styles from "./RelatedServicesItem.scss";
 
-type RelatedServicesItemProps = { logoUrl: string; url: string };
+type RelatedServicesItemProps = {
+  logoUrl: string;
+  url: string;
+  underDevelopment?: boolean;
+};
 
 export const RelatedServicesItem: React.FC<RelatedServicesItemProps> = ({
   logoUrl,
-  url
+  url,
+  underDevelopment = false
 }) => (
   <a href={url} target="_blank" rel="noopener noreferrer">
     <div className={styles.container}>
-      <img src={logoUrl} />
+      <img src={logoUrl} className={styles.logo} />
+      {underDevelopment && (
+        <img
+          src="/images/related-services/badge.svg"
+          className={styles.badge}
+        />
+      )}
     </div>
   </a>
 );
