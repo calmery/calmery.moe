d36a738160bcedd132b1056e035458cde4f6bc86
Marei Kikukawa <contact@calmery.me>
2020-04-08 23:55:20 +0900

    Sentry の DSN を変更する

--- src/helpers/sentry.ts
+++ src/helpers/sentry.ts
@@ -2,6 +2,7 @@ import * as Sentry from "@sentry/browser";
 
 if (process.env.NODE_ENV === "production") {
   Sentry.init({
-    dsn: "https://35c3c6bfa928420a80d0c15d8a7a1a34@sentry.io/1868485"
+    dsn:
+      "https://35c3c6bfa928420a80d0c15d8a7a1a34@o325460.ingest.sentry.io/1868485"
   });
 }
